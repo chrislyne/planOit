961848d53260564fdfa521e6730fa47d813a771b
Gareth <gareth@garethnz.com>
2018-07-20 22:39:09 +1200

    Temp GarethsPlanet Add a generator (WIP) DefineAShapeEquations

---
+++ planOit/Assets/GarethsPlanet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarethsPlanet : MonoBehaviour {
+
+    public List<GarethsPlanet> Children;
+
+    public Vector3 Location
+    {
+        get
+        {
+            return gameObject.transform.position;
+        }
+        set
+        {
+            gameObject.transform.position = value;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
