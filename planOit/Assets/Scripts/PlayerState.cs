6eb6e3b77361aab4f7a2a2dc7b47fa4ba631f155
Hiroto-N <hiroto.nakamura95@gmail.com>
2018-07-21 17:31:02 +1200

    rename Player to PlayerState

---
+++ planOit/Assets/Scripts/PlayerState.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour {
+
+    public int foodDepletionRate;
+    public int oxygenDepletionRate;
+    public int materialsDepletionRate;
+
+    public float resourceDepletionMultiplier;
+
+    public GameObject[] resourceBars;
+
+    public ResourceSet resources;
+
+    // Use this for initialization
+    void Start() {
+        resources = new ResourceSet(100, 100, 100, 100);
+        InvokeRepeating("ExpendResources", 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        resourceBars[0].GetComponent<RectTransform>().sizeDelta = new Vector2(resources.oxygen/10f, 1);
+        resourceBars[1].GetComponent<RectTransform>().sizeDelta = new Vector2(resources.food/10f, 1);
+        resourceBars[2].GetComponent<RectTransform>().sizeDelta = new Vector2(resources.fuel/10f, 1);
+        resourceBars[3].GetComponent<RectTransform>().sizeDelta = new Vector2(resources.materials/10f, 1);
+    }
+
+    void ExpendResources()
+    {
+        resources.oxygen -= Mathf.RoundToInt(oxygenDepletionRate*resourceDepletionMultiplier);
+        resources.food -= Mathf.RoundToInt(foodDepletionRate*resourceDepletionMultiplier);
+        resources.materials -= Mathf.RoundToInt(materialsDepletionRate*resourceDepletionMultiplier);
+    }
+
+    void SpendFuel(int amountToUse)
+    {
+        if (resources.fuel > amountToUse)
+        {
+            resources.fuel -= amountToUse;
+        }
+        else
+        {
+            //TODO: failure state?
+            print("No more fuel.");
+        }
+    }
+}
