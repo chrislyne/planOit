910640fd16485207773064bd451a2d47e1bbc940
Gareth <gareth@garethnz.com>
2018-07-22 13:10:18 +1200

    'P'lanet and eventManager

---
+++ planOit/Assets/Scripts/EventManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace planOit
+{
+    public enum PlanetEventType
+    {
+        UNSET = 0,
+        NOTHING,
+        NOTHING2,
+        NOTHING_OF_NOTE,
+        BONUS_RESOURCES,
+        BONUS_RESOURCES2,
+        RESOURCE_PENALTY,
+        ALIEN_ATTACK,
+        COUNT
+    }
+
+    public class EventManager
+    {
+        public static PlanetEventType getRandomEvent()
+        {
+            return (PlanetEventType)Random.Range(0, (int)PlanetEventType.COUNT);
+        }
+    }
+}
