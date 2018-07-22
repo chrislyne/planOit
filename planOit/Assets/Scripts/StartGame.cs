2fd4160e9ed5f63cc8a81e36fb3475e19a4db1fc
Chris Lyne <chris@funnylyne.com>
2018-07-22 15:38:27 +1200

    Menu screen

---
+++ planOit/Assets/Scripts/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+    public string level;
+
+	// Use this for initialization
+	public void StartTheGame () {
+        SceneManager.LoadScene(level);
+	}
+	
+	
+}
